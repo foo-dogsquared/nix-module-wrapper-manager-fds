aa20b8eacc4bed455d56d6e31727b9808075301f
Gabriel Arazas <foodogsquared@foodogsquared.one>
2025-06-06 15:00:38 +0800

    modules/env/nixos: enable systemd units installation from wrapper-manager packages

--- modules/env/nixos/default.nix
+++ modules/env/nixos/default.nix
@@ -15,6 +15,13 @@ in
 {
   imports = [ ../common.nix ];
 
+  options.wrapper-manager = {
+    enableInstallSystemdUnits =
+      lib.mkEnableOption "install systemd units from wrapper-manager packages" // {
+        default = true;
+      };
+  };
+
   config = lib.mkMerge [
     {
       environment.systemPackages =
@@ -32,15 +39,30 @@ in
             config.locale.enable = lib.mkDefault false;
           }
         )
+
+        (
+          { lib, ... }: {
+            options.enableInstallSystemdUnits = lib.mkEnableOption "install systemd units from wrapper-manager configuration" // {
+              default = cfg.enableInstallSystemdUnits;
+            };
+          }
+        )
       ];
     }
 
-    (lib.mkIf (cfg.packages != { }) {
-      environment.systemPackages =
-        let
-          validPackages = lib.filterAttrs (_: wrapper: wrapper.enableInstall) cfg.packages;
-        in
-        lib.mapAttrsToList (_: wrapper: wrapper.build.toplevel) validPackages;
-    })
+    (lib.mkIf (cfg.packages != { }) (
+      let
+        filterPackages = cond:
+          let
+            validPackages = lib.filterAttrs cond cfg.packages;
+          in
+          lib.mapAttrsToList (_: wrapper: wrapper.build.toplevel) validPackages;
+      in
+      {
+        environment.systemPackages = filterPackages (_: wrapper: wrapper.enableInstall);
+
+        systemd.packages = filterPackages (_: wrapper: wrapper.enableInstall && wrapper.enableInstallSystemdUnits);
+      }
+    ))
   ];
 }
